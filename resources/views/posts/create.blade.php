41f87bed39582c508f194b43001283cab3856929
ayuarista <aristaadewi8@gmail.com>
2025-03-20 21:14:15 +0800

    enhance post creation form styling and improve user feedback messages

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,15 +4,15 @@
         @csrf
         <div class="mb-4">
             <label class="block font-medium text-gray-900">Title</label>
-            <input type="text" name="title" class="w-full p-2 border rounded focus:outline-indigo-500" value="{{ old('title') }}" required>
+            <input type="text" name="title" class="w-full p-2 border rounded focus:outline-indigo-500 border-gray-900/25" value="{{ old('title') }}" required>
         </div>
         <div class="mb-4">
             <label class="block font-medium text-gray-900">Content</label>
-            <textarea name="body" class="w-full p-2 border rounded focus:outline-indigo-500" required>{{ old('body') }}</textarea>
+            <textarea name="body" class="w-full p-2 border rounded focus:outline-indigo-500 border-gray-900/25" required>{{ old('body') }}</textarea>
         </div>
         <div class="mb-4">
             <label class="block font-medium text-gray-900">Select Category</label>
-            <select name="category_id" id="category" class="w-full border focus:outline-indigo-500 p-2 rounded-md">
+            <select name="category_id" id="category" class="w-full mt-1 border-gray-900/25 border focus:outline-indigo-500 p-2 rounded-md">
                 @foreach ($categories as $category)
                     <option value="{{ $category->id }}">
                         {{ $category->name }}
@@ -23,12 +23,12 @@
         <div class="col-span-full">
             <div class="flex gap-2">
                 <label for="cover-photo" class="block font-medium text-gray-900">Thumbnail</label>
-                <p class="text-red-600 text-sm">*max size 2MB</p>
+                <p class="text-red-600 text-sm">*max file size 2MB</p>
             </div>
             <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                 <div class="mt-4 flex flex-col items-center text-sm text-gray-600">
                     <label for="imageInput"
-                        class="relative cursor-pointer rounded-md bg-white border border-gray-300 px-4 py-2 text-indigo-600 font-semibold hover:bg-gray-100 hover:text-indigo-500">
+                        class="relative cursor-pointer rounded-md bg-white border border-gray-300 px-4 py-2 text-indigo-600 font-semibold hover:bg-indigo-500 hover:text-white transition-all duration-300">
                         Choose Image
                     </label>
 
@@ -41,12 +41,12 @@
 
                     <img id="imagePreview" class="hidden w-40 h-40 object-cover mt-4 rounded-lg">
 
-                    <p id="fileError" class="text-red-500 text-sm hidden mt-2">File terlalu besar! Maksimal 2MB.</p>
+                    <p id="fileError" class="text-red-500 text-sm hidden mt-2">File is too large! 2MB maximum.</p>
                 </div>
             </div>
         </div>
 
-        <button type="submit" class="bg-blue-500 hover:bg-blue-600 cursor-pointer mt-3 w-full text-white px-4 py-2 rounded">
+        <button type="submit" class="bg-indigo-500 hover:bg-indigo-500/90 transition-all duration-300 cursor-pointer mt-3 w-full text-white px-4 py-2 rounded font-medium">
             Create Post
         </button>
     </form>
