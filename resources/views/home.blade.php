08c81bf5585febddd4804bc99732a81edfd2e93e
ayuarista <aristaadewi8@gmail.com>
2025-03-21 23:08:25 +0800

    enhance home view with improved layout and styling for better user engagement

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,33 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h1 class="text-xl text-black">Home</h1>
+    <div>
+        <h1 class="text-2xl font-bold text-black">Blog Website</h1>
+        <p class="text-gray-400 text-sm text-pretty">The latest and best lifestyle articles selected by our editorial office</p>
+    </div>
+    <div class="grid grid-cols-2">
+        <div class="grid col-span-2">
+            <div class="relative">
+                <img src="https://plus.unsplash.com/premium_photo-1675700427405-f4cf90589055?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full">
+                <div class="absolute top-3 left-2">
+                    <h1 class="font-bold text-xl text-white">Wake up and smell the coffe</h1>
+                </div>
+            </div>
+        </div>
+        <div class="grid grid-cols-1">
+            <div class="relative">
+                <img src="https://plus.unsplash.com/premium_photo-1675700427405-f4cf90589055?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full">
+                <div class="absolute top-3 left-2">
+                    <h1 class="font-bold text-xl text-white">Wake up and smell the coffe</h1>
+                </div>
+            </div>
+        </div>
+        <div class="grid grid-cols-1">
+            <div class="relative">
+                <img src="https://plus.unsplash.com/premium_photo-1675700427405-f4cf90589055?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full">
+                <div class="absolute top-3 left-2">
+                    <h1 class="font-bold text-xl text-white">Wake up and smell the coffe</h1>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layout>
