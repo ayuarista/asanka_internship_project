84a25c642f3376cbfaf695343eeec2d6ef24decb
ayuarista <aristaadewi8@gmail.com>
2025-03-22 22:03:34 +0800

    update home view images and text for improved visual appeal and readability

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,10 +42,10 @@
         <div class="grid grid-cols-2 gap-4 max-w-2xl mt-8">
             <div class="grid grid-cols-1">
                 <div class="relative">
-                    <img src="https://images.unsplash.com/photo-1531875456634-3f5418280d20?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full rounded-md">
-                    <div class="absolute bottom-3 left-8">
-                        <p class="text-sm text-emerald-700/80">INTERIOR</p>
-                        <h1 class="font-bold text-3xl text-emerald-700">9 ways to prepare new plants</h1>
+                    <img src="https://images.unsplash.com/photo-1730033135221-cf2da5ab25cb?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+                    <div class="absolute bottom-3 left-5">
+                        <p class="text-sm text-white/80">INTERIOR</p>
+                        <h1 class="font-bold text-3xl text-white">9 ways to prepare new plants</h1>
                     </div>
                 </div>
             </div>
@@ -61,11 +61,11 @@
             <div class="grid col-span-2">
                 <div class="relative">
                     <img src="https://images.unsplash.com/photo-1742317402143-449e8b4cbf91?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="rounded-md h-96 w-full">
-                    <div class="absolute top-8 left-8">
+                    <div class="absolute bottom-8 left-8">
                         <p class="text-sm text-white/80">FOOD</p>
                         <h1 class="font-bold text-4xl text-white">Wake up and smell <br> the coffee</h1>
                     </div>
-                    <div class="absolute bottom-10 left-8">
+                    <div class="absolute top-10 left-8">
                         <a href="/posts/create" class="px-8 py-2 font-medium rounded-md bg-white text-black hover:bg-white/80
                          transition-all duration-300">Create Blog</a>
                     </div>
