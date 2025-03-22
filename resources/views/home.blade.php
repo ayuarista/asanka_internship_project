a36e162e949cc2a984dee6b52acbc0c111b2846c
ayuarista <aristaadewi8@gmail.com>
2025-03-22 22:08:21 +0800

    update home view image for improved visual appeal and relevance

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,7 @@
             </div>
             <div class="grid grid-cols-1">
                 <div class="relative">
-                    <img src="https://images.unsplash.com/photo-1547469447-4afec158715b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full rounded-md shadown-xl">
+                    <img src="https://images.unsplash.com/photo-1594057720001-d1c340b36730?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full rounded-md shadown-xl">
                     <div class="absolute top-8 left-4">
                         <p class="text-sm text-white/80">EDUCATION</p>
                         <h1 class="font-bold text-3xl text-white">Time to learn anything</h1>
