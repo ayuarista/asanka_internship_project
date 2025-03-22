1d97e128048008b4e51d7a415db4c85a3d4624e1
ayuarista <aristaadewi8@gmail.com>
2025-03-22 22:17:38 +0800

    update home view images for enhanced visual appeal and relevance

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
         <div class="grid grid-cols-2 gap-4 max-w-2xl mt-8">
             <div class="grid col-span-2">
                 <div class="relative">
-                    <img src="https://images.unsplash.com/photo-1501843508755-af0829d48618?q=80&w=2106&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="rounded-md">
+                    <img src="https://images.unsplash.com/photo-1735889103738-90ed3c3e2255?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="rounded-md">
                     <div class="absolute top-8 left-8">
                         <p class="text-sm text-white/80">FOOD</p>
                         <h1 class="font-bold text-4xl text-white">Wake up and smell <br> the coffee</h1>
@@ -22,7 +22,7 @@
             </div>
             <div class="grid grid-cols-1">
                 <div class="relative">
-                    <img src="https://images.unsplash.com/photo-1531875456634-3f5418280d20?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full rounded-md">
+                    <img src="https://images.unsplash.com/photo-1722073624764-909fc7e3db50?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full rounded-md">
                     <div class="absolute bottom-3 left-8">
                         <p class="text-sm text-emerald-700/80">EDUCATION</p>
                         <h1 class="font-bold text-3xl text-emerald-700">Time to learn anything</h1>
@@ -38,11 +38,7 @@
         <div class="grid grid-cols-2 gap-4 max-w-2xl mt-8">
             <div class="grid grid-cols-1">
                 <div class="relative">
-                    <img src="https://plus.unsplash.com/premium_photo-1661311950994-d263ea9681a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full rounded-md shadown-xl">
-                    <div class="absolute top-8 left-4">
-                        <p class="text-sm text-emerald-800/80">TRAVELLING</p>
-                        <h1 class="font-bold text-3xl text-emerald-800">Top 3 Best Place to Travel</h1>
-                    </div>
+                    <img src="https://images.unsplash.com/photo-1543920926-1125e0a12243?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full rounded-md shadown-xl">
                 </div>
             </div>
             <div class="grid grid-cols-1">
