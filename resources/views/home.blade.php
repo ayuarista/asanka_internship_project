45f428097bf0d89bbdeca9f148e568540cbd7deb
ayuarista <aristaadewi8@gmail.com>
2025-03-22 22:23:20 +0800

    update home view layout and text for improved readability and visual consistency

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,11 @@
     <x-slot:title>{{ $title }}</x-slot:title>
     <div>
         <h1 class="text-4xl font-bold text-black">Blog Website</h1>
-        <p class="text-gray-400 text-sm text-pretty">The latest and best lifestyle articles selected by our editorial office</p>
+        <p class="text-gray-400 text-[15px] text-pretty mt-2 max-w-80">The latest and best lifestyle articles selected by our editorial office</p>
     </div>
 
-    <div class="flex items-center justify-center gap-4">
-        <div class="grid grid-cols-2 gap-4 max-w-2xl mt-8">
+    <div class="flex items-center justify-center gap-4 mt-4">
+        <div class="grid grid-cols-2 gap-4 max-w-2xl">
             <div class="grid col-span-2">
                 <div class="relative">
                     <img src="https://images.unsplash.com/photo-1735889103738-90ed3c3e2255?q=80&w=2128&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="rounded-md">
@@ -35,11 +35,11 @@
                 </div>
             </div>
         </div>
-        <div class="grid grid-cols-2 gap-4 max-w-2xl mt-8">
+        <div class="grid grid-cols-2 gap-4 max-w-2xl">
             <div class="grid grid-cols-1">
                 <div class="relative">
                     <img src="https://images.unsplash.com/photo-1543920926-1125e0a12243?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="w-full rounded-md shadown-xl">
-                    <div class="absolute top-10 left-5">
+                    <div class="absolute top-8 left-4">
                         <a href="/posts" class="px-8 py-2 font-medium rounded-md bg-black text-white hover:bg-black/80
                          transition-all duration-300">Read Blog</a>
                     </div>
